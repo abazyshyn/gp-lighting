fad82733a8d107cd8e4f010199f6684415273a1f
abazyshyn <maybebazishin@gmail.com>
2026-09-10 10:00:08 +0300

    feat: camera class

---
+++ lighting/src/system/application/camera/camera.h
@@ -0,0 +1,27 @@
+#pragma once
+
+namespace GP
+{
+
+    class CCamera
+    {
+    public:
+        CCamera();
+
+        void Render();
+
+        void SetPosition(float x, float y, float z) { m_position = XMFLOAT3(x, y, z); }
+        void SetRotation(float x, float y, float z) { m_rotation = XMFLOAT3(x, y, z); }
+        XMFLOAT3 GetPosition() const { return m_position; }
+        XMFLOAT3 GetRotation() const { return m_rotation; }
+        void GetViewMatrix(XMMATRIX &viewMatrix) const { viewMatrix = m_viewMatrix; }
+
+    private:
+        XMMATRIX m_viewMatrix;
+        XMFLOAT3 m_position;
+        XMFLOAT3 m_rotation;
+
+        uint_least8_t _pad[8] = {};
+    };
+
+} // namespace GP
